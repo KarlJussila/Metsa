8f3462e8762d401acb5b3b4a4145a59480c40621
KarlJussila <kjussila@mac.com>
2022-09-10 11:54:35 -0500

    Small changes

--- Player/Light2D.gd
+++ Player/Light2D.gd
@@ -1,5 +1,8 @@
 extends Light2D
 
+var intensity = self.energy
+
 func _process(delta):
 	if Input.is_action_just_pressed("light"):
-		self.energy = 1.0 - self.energy
+		self.intensity = 1.0 - self.intensity
+		self.energy = self.intensity
