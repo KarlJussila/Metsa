488160ce4ea9f57037294d70056b2c66aab0d0d4
KarlJussila <kjussila@mac.com>
2022-09-24 14:23:29 -0500

    Removed debug messages

--- World/Interactable.gd
+++ World/Interactable.gd
@@ -11,12 +11,10 @@ var player_in_range : bool = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if player_in_range and mouse_in_area and Input.is_action_just_pressed("interact"):
-		print("Interacted 1")
 		emit_signal("interacted")
 
 func _on_InteractArea_mouse_entered():
 	mouse_in_area = true
-	print("mouse")
 
 func _on_InteractArea_mouse_exited():
 	mouse_in_area = false
@@ -24,7 +22,6 @@ func _on_InteractArea_mouse_exited():
 func _on_InteractRange_area_entered(area):
 	if (area.get_name() == "PlayerCollisionArea"):
 		player_in_range = true
-		print("player")
 		
 func _on_InteractRange_area_exited(area):
 	if (area.get_name() == "PlayerCollisionArea"):
